4a450101695f105e174a18a390e00ae58ac73e3a
Esteban Vivanco <vivancoesteban17@gmail.com>
2024-06-22 19:57:02 -0300

    cosas

--- src/main/java/aed/SistemaSIU.java
+++ src/main/java/aed/SistemaSIU.java
@@ -8,7 +8,7 @@ public class SistemaSIU {// cuando le pasas el lu te devuelve el nombre del estu
 
     private ArbolTrie libretasArbolTrie;
     private ArbolTrie materiasArbolTrie;
-    private dict<String,Alumno> luAAlumno;
+    private dict<String,Alumno> luAAlumno = new dict<>(0);
     private dict<String,Materia> datosMateria;
     private InfoMateria[] ListaMaterias;
     private String[] ListalibretasUniversitarias;
@@ -27,7 +27,7 @@ public class SistemaSIU {// cuando le pasas el lu te devuelve el nombre del estu
     }
 
     public void inscribir(String estudiante, String carrera, String materia){
-        if (this.libretasArbolTrie.buscar(estudiante) == True){
+        if (this.libretasArbolTrie.buscar(estudiante) ==  true){
             this.luAAlumno[estudiante].sumarMateria();
             this.cantidadDeInscriptosMateria[materia].sumarInsciptos();
         }
@@ -72,23 +72,15 @@ public class SistemaSIU {// cuando le pasas el lu te devuelve el nombre del estu
         }
         return todasLasCarreras.toArray(new String[0]);	    
     }
-    public Boolean pertence(T[] s, T e) {
-        for( T t : s){
-            if (t == e){
-                return True;
-            }
-        }
-        return False;
-    }
 
     public String[] materias(String carrera){
         Set<String> todasLasMaterias = new HashSet<>();
-        for(ParCarreraMateria par : this.infoMaterias){
+        for(ParCarreraMateria par : this.ListaMaterias){
             if (carrera == par.getCarrera()){
                 todasLasMaterias.add(par.getNombreMateria());
             }
         }
-        return todasLasCarreras.toArray(new String[0]);;	    
+        return ListaMaterias.toArray(new String[0]);;	    
     }
 
     public int materiasInscriptas(String estudiante){
